60920dc934436aa0af2c6f9d83d3307bfad851e4
soufiane-artisrt <mbank.globe@gmail.com>
2026-04-08 22:47:25 +0100

    dsfsd

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 // باش نخلي public فولدر accessible
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen('https://test-alpha-eight-80.vercel.app/', () => {
+app.listen(PORT, () => {
     console.log(`Server khdam f http://localhost:${PORT}`);
 });
